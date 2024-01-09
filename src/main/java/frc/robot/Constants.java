0cf31faa0fe0af1ab83584ec6a672ea896a286b4
David <davben44@gmail.com>
2024-01-08 18:20:52 -0800

    PID tuned to fix overshooting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,15 +15,15 @@ public class Constants {
         public static final double wheelRadiusMeters = 0.051;
 
         public static final int gyroCanID = 7;
-        public static final double maxOutputPercentage = 0.7;
+        public static final double maxOutputPercentage = 1;
 
         // PID tunes for 51.5 pounds 
-        public static final double drivingPID_P = 0.05;
+        public static final double drivingPID_P = 0.03;
         public static final double drivingPID_I = 0;
         public static final double drivingPID_D = 0;
 
        // PID tunes for 51.5 pounds 
-        public static final double turningPID_P = 2.45; 
+        public static final double turningPID_P = 1.45; 
         public static final double turningPID_I = 0;
         public static final double turningPID_D = 0.32;
 
