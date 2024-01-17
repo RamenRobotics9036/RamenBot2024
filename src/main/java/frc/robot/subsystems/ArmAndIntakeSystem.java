e2a785cd2927ef9b581fffaa901184c03c7c9818
Ido <idoben2@hotmail.com>
2024-01-16 22:32:10 -0800

    Clean style warnings

--- src/main/java/frc/robot/subsystems/ArmAndIntakeSystem.java
+++ src/main/java/frc/robot/subsystems/ArmAndIntakeSystem.java
@@ -4,37 +4,13 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.ErrorCode;
-import com.ctre.phoenix.sensors.Pigeon2;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.CAN;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.IntakeConstants;
-import frc.robot.Constants.ShooterConstants;
-import frc.robot.Constants.SwerveSystemConstants;
-import frc.robot.Constants.SwerveSystemConstants.SwerveSystemDeviceConstants;
-import frc.robot.commands.DriveSwerveCommand;
-import frc.robot.util.AppliedController;
-import java.util.Map;
-import java.util.function.DoubleSupplier;
 
 /**
  * SwerveDriveSystem.
@@ -45,19 +21,19 @@ public class ArmAndIntakeSystem extends SubsystemBase {
             MotorType.kBrushless);
     public final CANSparkMax m_armMotor = new CANSparkMax(ArmConstants.armMotorID,
             MotorType.kBrushless);
-    public final DutyCycleEncoder m_ArmEncoder = new DutyCycleEncoder(
+    public final DutyCycleEncoder m_armEncoder = new DutyCycleEncoder(
             ArmConstants.armEncoderChannel);
     // public DigitalInput refelectometer = new DigitalInput(ShooterConstants.reflectChannel);
-    public double Speed;
+    public double m_speed;
 
     public ArmAndIntakeSystem() {
         initShuffleBoard();
 
     }
 
-    public void setIntakeSpeed(double SPEED) {
-        Speed = SPEED;
-        m_intakeMotor.set(Speed);
+    public void setIntakeSpeed(double speed) {
+        m_speed = speed;
+        m_intakeMotor.set(m_speed);
     }
 
     public double getIntakeSpeed() {
@@ -66,7 +42,7 @@ public class ArmAndIntakeSystem extends SubsystemBase {
     }
 
     public double getArmangle() {
-        return m_ArmEncoder.getAbsolutePosition();
+        return m_armEncoder.getAbsolutePosition();
 
     }
 
@@ -77,10 +53,13 @@ public class ArmAndIntakeSystem extends SubsystemBase {
 
     }
 
+    /**
+     * Initialize the Shuffleboard.
+     */
     public void initShuffleBoard() {
 
         Shuffleboard.getTab("Arm and Intake").add("Arm Angle: ",
-                m_ArmEncoder.getAbsolutePosition());
+                m_armEncoder.getAbsolutePosition());
         Shuffleboard.getTab("Arm and Intake").add("Intake Speed: ", getIntakeSpeed());
 
     }
