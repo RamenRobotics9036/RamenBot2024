0554ab8771544a32f8d5a29a87953597bbb86a3a
Ramen Programmer <idoben2@hotmail.com>
2024-01-26 17:00:56 -0800

    Added commands for driving one variable at a time.

---
+++ src/main/java/frc/robot/commands/DriveRotCommand.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.SwerveDriveSystem;
+
+public class DriveRotCommand extends CommandBase {
+    private SwerveDriveSystem m_swerveSystem;
+    private double m_speed;
+    private double m_maxTime;
+    private Timer m_timer;
+
+    public DriveRotCommand(SwerveDriveSystem swerveSystem, double speed, double maxSpeed) {
+        m_swerveSystem = swerveSystem;
+        m_speed = speed;
+        m_timer = new Timer();
+        m_maxTime = maxSpeed;
+        addRequirements(m_swerveSystem);
+    }
+
+
+    @Override
+    public void initialize() {
+        m_timer.start();
+    }
+
+    @Override
+    public void execute() {
+        m_swerveSystem.drive(0, 0, m_speed, true);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (m_timer.get() >= m_maxTime){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_swerveSystem.stopSystem();
+    }
+}
