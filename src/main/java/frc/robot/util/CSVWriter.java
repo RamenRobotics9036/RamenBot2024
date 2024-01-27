f71e05103583446e6a078bab505ef88c40304c13
RamenRoboticsProgrammer <ramenroboticsprogrammer@gmail.com>
2024-01-26 16:41:36 -0800

    Added an OpenCSV test class

--- src/main/java/frc/robot/util/CSVWriter.java
+++ src/main/java/frc/robot/util/CSVWriter.java
@@ -1,5 +1,26 @@
 package frc.robot.util;
 
-public class CSVWriter {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
+import com.opencsv.CSVWriter;
+
+public class CsvWriter {
+
+    public static void Datawrite() throws IOException {
+
+        String[] entries = {
+                "book", "coin", "pencil", "cup"
+        };
+        String fileName = "src/main/resources/items.csv";
+
+        try (var fos = new FileOutputStream(fileName);
+                var osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+                var writer = new CSVWriter(osw)) {
+
+            writer.writeNext(entries);
+        }
+    }
 }
