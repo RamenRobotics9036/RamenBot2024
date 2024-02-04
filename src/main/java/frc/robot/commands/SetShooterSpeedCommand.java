bb50baa157c7d21d9221ef6398e0a17c8c091a79
Ramen <ramenrobotics0@gmail.com>
2024-02-03 16:23:55 -0800

    Fixed pulling in notes.

---
+++ src/main/java/frc/robot/commands/SetShooterSpeedCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSystem;
+
+public class SetShooterSpeedCommand extends CommandBase {
+    private ShooterSystem m_shooterSystem;
+    private double m_maxTime;
+    private double m_speed;
+    private Timer m_timer;
+
+    public SetShooterSpeedCommand(ShooterSystem shooterSystem, double maxTime, double speed) {
+        m_shooterSystem = shooterSystem;
+        m_maxTime = maxTime;
+        m_speed = speed;
+        addRequirements(m_shooterSystem);
+    }
+
+    @Override
+    public void initialize() {
+        m_timer = new Timer();
+        m_timer.start();
+    }
+
+    @Override
+    public void execute() {
+        m_shooterSystem.setShootSpeed(m_speed);
+    }
+
+    @Override
+    public boolean isFinished() {
+        if (m_timer.get() >= m_maxTime) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_shooterSystem.stopSystem();
+    }
+}
