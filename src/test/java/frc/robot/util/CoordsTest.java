4518d124199d76c08ee8c60794d58de6ddc8fb80
Ido <idoben2@hotmail.com>
2024-02-10 15:07:05 -0800

    Add changes back that were accidentally reverted
    Add changes back that were accidentally reverted
    
    Add placeholder unit test back

---
+++ src/test/java/frc/robot/util/CoordsTest.java
@@ -0,0 +1,17 @@
+package frc.robot.util;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CoordsTest {
+    @BeforeEach
+    void setup() {
+    }
+
+    @Test
+    public void testShouldSucceed() {
+        assertTrue(true);
+    }
+}
