9922d5865cd7066df2cf27277fdabafd6dd724c6
Ido <idoben2@hotmail.com>
2024-02-25 19:10:19 -0800

    Get Simulation Field up

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import java.util.function.Supplier;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
@@ -11,6 +13,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
@@ -34,6 +37,10 @@ import frc.robot.subsystems.SwerveDriveSystem;
 import frc.robot.subsystems.SwerveDriveSystemSim;
 import frc.robot.subsystems.VisionSystem;
 import frc.robot.util.AppliedController;
+import simulationlib.shuffle.MultiType;
+import simulationlib.shuffle.PrefixedConcurrentMap;
+import simulationlib.shuffle.ShuffleboardHelpers;
+import simulationlib.shuffle.SupplierMapFactory;
 
 /**
  * RobotContainer.
@@ -52,6 +59,8 @@ public class RobotContainer {
     private ArmSystem m_armSystem = new ArmSystem(m_armController);
     private IntakeSystem m_intakeSystem = new IntakeSystem();
     private HookSystem m_hookSystem = new HookSystem(m_armController);
+
+    private PopulateSimShuffleboard m_shuffleboardManager = null;
 
     public RobotContainer() {
         double waitTime = 0.2;
@@ -75,6 +84,33 @@ public class RobotContainer {
                                         new IntakeRevCommand(m_intakeSystem, m_shooterSystem,
                                                 m_armController)),
                         new StayCommand(m_swerveDrive)));
+        if (RobotBase.isSimulation()) {
+            initSimShuffleboard();
+        }
+    }
+
+    private void initSimShuffleboard() {
+        // Now that all subsystems are created, print out the list of properties
+        // available for display in Shuffleboard.
+        printAvailableDashboardProperties();
+
+        m_shuffleboardManager = new PopulateSimShuffleboard(
+                new ShuffleboardHelpers(SupplierMapFactory.getGlobalInstance()),
+                new DefaultSimLayout(),
+                Shuffleboard.getTab("Simulation"));
+    }
+
+    private void printAvailableDashboardProperties() {
+        PrefixedConcurrentMap<Supplier<MultiType>> globalMap = SupplierMapFactory
+                .getGlobalInstance();
+
+        globalMap.prettyPrint();
+    }
+
+    public void updateSimShuffleboard() {
+        if (m_shuffleboardManager != null) {
+            m_shuffleboardManager.updateDashOnRobotPeriodic();
+        }
     }
 
     public void scheduleAutonomousCommand() {
@@ -111,6 +147,11 @@ public class RobotContainer {
                 "Angle to Shoot",
                 () -> m_armSystem.getShootingAngle(m_visionSystem.getDistanceMetersY())
                         + ShooterConstants.shootOffsetLimeLight);
+
+        if (m_shuffleboardManager != null) {
+            m_shuffleboardManager.addShuffleboardWidgets();
+            m_shuffleboardManager.addMacros(m_armSystem);
+        }
     }
 
     /**
