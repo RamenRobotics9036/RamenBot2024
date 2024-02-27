e6d321d89238b8b937528db832357dd640453d5b
Ido <idoben2@hotmail.com>
2024-02-26 23:20:09 -0800

    Sim overrides chassisSpeeds
    Abstract getChassisSpeeds so it can be overridden by subclass
    
    Sim passes back chassisSpeeds

--- src/main/java/frc/robot/sim/SwerveDriveSystemSim.java
+++ src/main/java/frc/robot/sim/SwerveDriveSystemSim.java
@@ -5,6 +5,7 @@ import static simulationlib.simulation.swerve.SwerveSimConstants.Swerve.kModuleT
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -165,6 +166,16 @@ public class SwerveDriveSystemSim extends SwerveDriveSystem {
         }
         else {
             return new Rotation2d();
+        }
+    }
+
+    @Override
+    public ChassisSpeeds getChassisSpeeds() {
+        if (m_simSwerveDrive != null) {
+            return m_simSwerveDrive.getChassisSpeeds();
+        }
+        else {
+            return new ChassisSpeeds();
         }
     }
 
