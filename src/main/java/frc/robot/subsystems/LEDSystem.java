717a26f106a6e95aa0657315df62af132e2206e8
RamenRoboticsProgrammer <ramenroboticsprogrammer@gmail.com>
2024-03-01 23:50:20 -0800

    Adds More pseudo code that I need to finish tomorrow when I can test with the robot.

--- src/main/java/frc/robot/subsystems/LEDSystem.java
+++ src/main/java/frc/robot/subsystems/LEDSystem.java
@@ -1,9 +1,5 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -44,7 +40,7 @@ public class LEDSystem extends SubsystemBase {
         // initShuffleBoard();
 
         m_ledLoop = 0;
-        m_ledR = 0;
+        m_ledR = 255;
         m_ledG = 0;
         m_ledB = 0;
         m_ledHue = 0;
@@ -82,40 +78,21 @@ public class LEDSystem extends SubsystemBase {
         // FIND WHAT THE NORMALCURRENT OF THE MOTOR RUNS AT TOMORROW
         // SEE IF THERE IS A SPIKE WHEN IT IS INTAKING A PIECE (HOLD THE PIECE SO THE INTAKE CANNOT
         // FULLY GRAB IT)
+        if (m_intakeSystem.getOutputCurrent() > NORMALCURRENT) {
+            // m_LEDBuffer.setHSV()
+            m_LEDBuffer.setRGB(0, 0, 255, 0); // WILL ONLY CHANGE ONE LIGHT FOR NOW. ADD A FOR LOOP
+                                              // TO ACTUALLY CHANGE ALL OF THE LIGHTS LATER
+
+        }
 
         // THINK ABOUT HOW IT WILL KNOW IF IT DOESNT HAVE A PIECE
         // for now, when it shoots, thats when it will know that it used a piece. I can check this
         // by other checking the velocity of the shoot motors or by seeing if the shoot command was
         // run
 
-        if (m_intakeSystem.getOutputCurrent() > NORMALCURRENT) {
-            // m_LEDBuffer.setHSV()
-        }
+        // If shootsystem.usedcommand() == True {m_LEDBuffer.setRGB(0, 255, 0, 0)};
 
-        if (0 == m_ledR && 0 == m_ledG && 0 == m_ledB) {
-            for (var i = 0; i < m_LEDBuffer.getLength() / 2; i++) {
-                var hue = (m_ledHue + (i * 180 / (m_LEDBuffer.getLength() / 2))) % 180;
-                m_LEDBuffer.setHSV(i, hue, 255, 128);
-                m_LEDBuffer.setHSV(m_LEDBuffer.getLength() - i - 1, hue, 255, 128);
-            }
-            m_ledHue += 2;
-            m_ledHue %= 180;
-
-        }
-        else
-
-        {
-            for (var i = 0; i < m_LEDBuffer.getLength() / 2; i++) {
-                if (i == m_ledLoop) {
-                    m_LEDBuffer.setRGB(i, 0, 0, 0);
-                    m_LEDBuffer.setRGB(m_LEDBuffer.getLength() - i - 1, 0, 0, 0);
-                }
-                else {
-                    m_LEDBuffer.setRGB(i, m_ledG, m_ledR, m_ledB);
-                    m_LEDBuffer.setRGB(m_LEDBuffer.getLength() - i - 1, m_ledG, m_ledR, m_ledB);
-                }
-            }
-        }
+        // I WILL ALSO HAVE TO THINK ABOUT THE INTERACTION WITH THE AMP LIGHT
 
         m_LEDLight.setData(m_LEDBuffer);
         m_LEDLight.start();
@@ -125,6 +102,8 @@ public class LEDSystem extends SubsystemBase {
      * Stop the intake system.
      */
     public void stopSystem() {
-
+        m_LEDBuffer.setRGB(0, 0, 0, 0);
+        m_LEDLight.setData(m_LEDBuffer);
+        m_LEDLight.start();
     }
 }
